35e963e1609366e2f5ca71634324945651e7b936
sparshramchandani-NEU <ramchandani.sp@northeastern.edu>
2023-11-08 15:44:56 -0500

    Merging to the org main branch

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 const pulumi = require("@pulumi/pulumi");
 const aws = require("@pulumi/aws");
+const route53 = require("@pulumi/aws/route53");
+
 const config = new pulumi.Config("aws");
 const configure = new pulumi.Config("assignment-4");
 const rds = new pulumi.Config("rds");
+const route53Config = new pulumi.Config("route53");
 
 const strOwners = new pulumi.Config("ami").require("owners");
 const owners = strOwners.split();
-
 
 const selectedRegion = config.require("region");
 const vpcCidrBlock = configure.require("vpcCidrBlock");
@@ -14,211 +16,294 @@ const selectedProfile = config.require("profile");
 
 const keyName = new pulumi.Config("keyName").require("key");
 
-var splittedCidrBlock = vpcCidrBlock.split('/');
+var splittedCidrBlock = vpcCidrBlock.split("/");
 var vpcMask = parseInt(splittedCidrBlock[1]);
-const vpcCidrParts = splittedCidrBlock[0].split('.');
+const vpcCidrParts = splittedCidrBlock[0].split(".");
 // console.log(selectedProfile);
 
 const vpc = new aws.ec2.Vpc("myVpc", {
-    cidrBlock: vpcCidrBlock,
-    tags:{
-        Name: `${selectedProfile}-vpc`
-    }
+  cidrBlock: vpcCidrBlock,
+  tags: {
+    Name: `${selectedProfile}-vpc`,
+  },
 });
 
 const createVpcAndSubnets = async () => {
-    const azs = await aws.getAvailabilityZones({ state: 'available', region: selectedRegion });
-    const availabilityZones = azs.names.slice(0, azs.names.length < 3 ? azs.names.length : 3);
+  const azs = await aws.getAvailabilityZones({
+    state: "available",
+    region: selectedRegion,
+  });
+  const availabilityZones = azs.names.slice(
+    0,
+    azs.names.length < 3 ? azs.names.length : 3
+  );
 
-    const { publicSubnets, privateSubnets } = availabilityZones.reduce((result: { publicSubnets: any[]; privateSubnets: any[]; }, az: any, index: number) => {
-        const publicSubnet = new aws.ec2.Subnet(`publicSubnet${index}`, {
-            vpcId: vpc.id,
-            availabilityZone: az,
-            cidrBlock: `${vpcCidrParts[0]}.${vpcCidrParts[1]}.${index}.${vpcCidrParts[3]}/${vpcMask+8}`,
-            mapPublicIpOnLaunch: true,
-            tags:
-            {
-                Name: `${selectedProfile}-publicSubnet${index}`
-            }
-        });
-        const privateSubnet = new aws.ec2.Subnet(`privateSubnet${index}`, {
-            vpcId: vpc.id,
-            availabilityZone: az,
-            cidrBlock: `${vpcCidrParts[0]}.${vpcCidrParts[1]}.${index + 3}.${vpcCidrParts[3]}/${vpcMask+8}`,
-            tags:
-            {
-                Name: `${selectedProfile}-privateSubnet${index+3}`
-            }
-        });
-
-        result.publicSubnets.push(publicSubnet);
-        result.privateSubnets.push(privateSubnet);
-
-        return result;
-    }, { publicSubnets: [], privateSubnets: [] });
-
-    const internetGateway = new aws.ec2.InternetGateway("myInternetGateway", {
+  const { publicSubnets, privateSubnets } = availabilityZones.reduce(
+    (
+      result: { publicSubnets: any[]; privateSubnets: any[] },
+      az: any,
+      index: number
+    ) => {
+      const publicSubnet = new aws.ec2.Subnet(`publicSubnet${index}`, {
         vpcId: vpc.id,
-        tags:
-        {
-            Name: `${selectedProfile}-internetGateway`
-        }
-    });
-
-    const [publicRouteTable, privateRouteTable] = ['public', 'private'].map(tableType => {
-        return new aws.ec2.RouteTable(`${tableType}RouteTable`, {
-            vpcId: vpc.id,
-        });
-    });
-
-    const createRoute = (routeTableName: string, destinationCidrBlock: string, gatewayId: any) => {
-        return new aws.ec2.Route(`${routeTableName}Route`, {
-            routeTableId: routeTableName === 'public' ? publicRouteTable.id : privateRouteTable.id,
-            destinationCidrBlock,
-            gatewayId,
-        });
-    };
-
-    const createRouteTableAssociation = (subnet: { id: any; }, routeTableName: string, index: any) => {
-        return new aws.ec2.RouteTableAssociation(`${routeTableName}RTAssociation${index}`, {
-            subnetId: subnet.id,
-            routeTableId: routeTableName === 'public' ? publicRouteTable.id : privateRouteTable.id,
-        });
-    };
-
-    availabilityZones.forEach((az: any, index: string | number) => {
-        createRouteTableAssociation(publicSubnets[index], 'public', index);
-        createRouteTableAssociation(privateSubnets[index], 'private', index);
-    });
-
-    createRoute('public', '0.0.0.0/0', internetGateway.id);
-
-    const securityGroup = new aws.ec2.SecurityGroup("application-security-group", {
-        vpcId: vpc.id,
-        ingress: [
-            {
-                cidrBlocks: ["0.0.0.0/0"],
-                protocol: "tcp",
-                fromPort: 22,
-                toPort: 22,
-            },
-            {
-                cidrBlocks: ["0.0.0.0/0"],
-                protocol: "tcp",
-                fromPort: 80,
-                toPort: 80,
-            },
-            {
-                cidrBlocks: ["0.0.0.0/0"],
-                protocol: "tcp",
-                fromPort: 443,
-                toPort: 443,
-            },
-            {
-                cidrBlocks: ["0.0.0.0/0"],
-                protocol: "tcp",
-                fromPort: 3000,
-                toPort: 3000,
-            },
-        ],
-        egress: [
-            {
-              protocol: "-1", // All
-              fromPort: 0,
-              toPort: 0,
-              cidrBlocks: ["0.0.0.0/0"],
-            },
-          ],
-    });
-    
-    
-
-    const rdsSecurityGroup = new aws.ec2.SecurityGroup("sgRDS", {
-        description: "csye 6225RDS security group",
-        vpcId: vpc.id,
-        ingress: [
-          {
-            protocol: "tcp",
-            fromPort: 5432,
-            toPort: 5432,
-            securityGroups: [securityGroup.id],
-          },
-        ],
-        egress: [
-          {
-            protocol: "-1", // All
-            fromPort: 0,
-            toPort: 0,
-            cidrBlocks: ["0.0.0.0/0"],
-          },
-        ],
-      });
-      const dbParameterGroup = new aws.rds.ParameterGroup(
-        "db-p-group",
-        {
-          family: "postgres15",
-          description: "Custom parameter group for csye6225"
-        }
-      );
-      const dbSubnetGroup = new aws.rds.SubnetGroup("dbsubnetG", {
-        subnetIds: privateSubnets.map((subnet: { id: any; }) => subnet.id),
-        name: "csye6225",
+        availabilityZone: az,
+        cidrBlock: `${vpcCidrParts[0]}.${vpcCidrParts[1]}.${index}.${
+          vpcCidrParts[3]
+        }/${vpcMask + 8}`,
+        mapPublicIpOnLaunch: true,
         tags: {
-          Name: "csye6225",
+          Name: `${selectedProfile}-publicSubnet${index}`,
         },
       });
-      const rdsInstance = new aws.rds.Instance("csye6225db-instance", {
-        engine: rds.require("engine"),
-        instanceClass: rds.require("instanceClass"),
-        engineVersion: rds.require("engineVersion"),
-        allocatedStorage: rds.require("allocatedStorage"),
-        parameterGroupName: dbParameterGroup.name,
-        storageType: rds.require("storageType"),
-        dbName: rds.require("dbName"),
-        username: rds.require("username"),
-        password: rds.require("password"),
-        skipFinalSnapshot: true,
-        vpcSecurityGroupIds: [rdsSecurityGroup.id],
-        dbSubnetGroupName: dbSubnetGroup.name,
-        publiclyAccessible: false,
+      const privateSubnet = new aws.ec2.Subnet(`privateSubnet${index}`, {
+        vpcId: vpc.id,
+        availabilityZone: az,
+        cidrBlock: `${vpcCidrParts[0]}.${vpcCidrParts[1]}.${index + 3}.${
+          vpcCidrParts[3]
+        }/${vpcMask + 8}`,
+        tags: {
+          Name: `${selectedProfile}-privateSubnet${index + 3}`,
+        },
       });
 
+      result.publicSubnets.push(publicSubnet);
+      result.privateSubnets.push(privateSubnet);
 
-      const ami = pulumi.output(aws.ec2.getAmi({
-        owners: owners,
-        mostRecent: true,
-    }));
+      return result;
+    },
+    { publicSubnets: [], privateSubnets: [] }
+  );
 
-    console.log(ami.id);
-    
-    const instance = new aws.ec2.Instance("web-app-server", {
-        ami: ami.id,
-        userData: pulumi.interpolate `#!/bin/bash
+  const internetGateway = new aws.ec2.InternetGateway("myInternetGateway", {
+    vpcId: vpc.id,
+    tags: {
+      Name: `${selectedProfile}-internetGateway`,
+    },
+  });
+
+  const [publicRouteTable, privateRouteTable] = ["public", "private"].map(
+    (tableType) => {
+      return new aws.ec2.RouteTable(`${tableType}RouteTable`, {
+        vpcId: vpc.id,
+      });
+    }
+  );
+
+  const createRoute = (
+    routeTableName: string,
+    destinationCidrBlock: string,
+    gatewayId: any
+  ) => {
+    return new aws.ec2.Route(`${routeTableName}Route`, {
+      routeTableId:
+        routeTableName === "public"
+          ? publicRouteTable.id
+          : privateRouteTable.id,
+      destinationCidrBlock,
+      gatewayId,
+    });
+  };
+
+  const createRouteTableAssociation = (
+    subnet: { id: any },
+    routeTableName: string,
+    index: any
+  ) => {
+    return new aws.ec2.RouteTableAssociation(
+      `${routeTableName}RTAssociation${index}`,
+      {
+        subnetId: subnet.id,
+        routeTableId:
+          routeTableName === "public"
+            ? publicRouteTable.id
+            : privateRouteTable.id,
+      }
+    );
+  };
+
+  availabilityZones.forEach((az: any, index: string | number) => {
+    createRouteTableAssociation(publicSubnets[index], "public", index);
+    createRouteTableAssociation(privateSubnets[index], "private", index);
+  });
+
+  createRoute("public", "0.0.0.0/0", internetGateway.id);
+
+  //Creating CloudWatch Agent Role
+  const ec2Role = new aws.iam.Role("ec2Role", {
+    assumeRolePolicy: JSON.stringify({
+      Version: "2012-10-17",
+      Statement: [
+        {
+          Action: "sts:AssumeRole",
+          Effect: "Allow",
+          Principal: {
+            Service: "ec2.amazonaws.com",
+          },
+        },
+      ],
+    }),
+  });
+
+  const rolePolicyAttachment = new aws.iam.RolePolicyAttachment(
+    "rolePolicyAttachment",
+    {
+      policyArn: "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy",
+      role: ec2Role.name,
+    }
+  );
+
+  const instanceProfile = new aws.iam.InstanceProfile("instanceProfile", {
+    role: ec2Role.name,
+  });
+
+  const securityGroup = new aws.ec2.SecurityGroup(
+    "application-security-group",
+    {
+      vpcId: vpc.id,
+      ingress: [
+        {
+          cidrBlocks: ["0.0.0.0/0"],
+          protocol: "tcp",
+          fromPort: 22,
+          toPort: 22,
+        },
+        {
+          cidrBlocks: ["0.0.0.0/0"],
+          protocol: "tcp",
+          fromPort: 80,
+          toPort: 80,
+        },
+        {
+          cidrBlocks: ["0.0.0.0/0"],
+          protocol: "tcp",
+          fromPort: 443,
+          toPort: 443,
+        },
+        {
+          cidrBlocks: ["0.0.0.0/0"],
+          protocol: "tcp",
+          fromPort: 3000,
+          toPort: 3000,
+        },
+      ],
+      egress: [
+        {
+          protocol: "-1", // All
+          fromPort: 0,
+          toPort: 0,
+          cidrBlocks: ["0.0.0.0/0"],
+        },
+      ],
+    }
+  );
+
+  const rdsSecurityGroup = new aws.ec2.SecurityGroup("sgRDS", {
+    description: "csye 6225RDS security group",
+    vpcId: vpc.id,
+    ingress: [
+      {
+        protocol: "tcp",
+        fromPort: 5432,
+        toPort: 5432,
+        securityGroups: [securityGroup.id],
+      },
+    ],
+    egress: [
+      {
+        protocol: "-1", // All
+        fromPort: 0,
+        toPort: 0,
+        cidrBlocks: ["0.0.0.0/0"],
+      },
+    ],
+  });
+  const dbParameterGroup = new aws.rds.ParameterGroup("db-p-group", {
+    family: "postgres15",
+    description: "Custom parameter group for csye6225",
+  });
+  const dbSubnetGroup = new aws.rds.SubnetGroup("dbsubnetG", {
+    subnetIds: privateSubnets.map((subnet: { id: any }) => subnet.id),
+    name: "csye6225",
+    tags: {
+      Name: "csye6225",
+    },
+  });
+  const rdsInstance = new aws.rds.Instance("csye6225db-instance", {
+    engine: rds.require("engine"),
+    instanceClass: rds.require("instanceClass"),
+    engineVersion: rds.require("engineVersion"),
+    allocatedStorage: rds.require("allocatedStorage"),
+    parameterGroupName: dbParameterGroup.name,
+    storageType: rds.require("storageType"),
+    dbName: rds.require("dbName"),
+    username: rds.require("username"),
+    password: rds.require("password"),
+    skipFinalSnapshot: true,
+    vpcSecurityGroupIds: [rdsSecurityGroup.id],
+    dbSubnetGroupName: dbSubnetGroup.name,
+    publiclyAccessible: false,
+  });
+
+  const ami = pulumi.output(
+    aws.ec2.getAmi({
+      owners: owners,
+      mostRecent: true,
+    })
+  );
+
+  console.log(ami.id);
+
+  const instance = new aws.ec2.Instance("web-app-server", {
+    ami: ami.id,
+    userData: pulumi.interpolate`#!/bin/bash
         sudo rm /opt/csye6225/web-app/.env
         sudo touch /opt/csye6225/web-app/.env
-        sudo echo PGPORT=${rds.require('port')} >> /opt/csye6225/web-app/.env
+        sudo echo ENVIRONMENT=${config.require("profile")} >> /opt/csye6225/web-app/.env
+        sudo echo PGPORT=${rds.require("port")} >> /opt/csye6225/web-app/.env
         sudo echo PGUSER=${rds.require("username")} >> /opt/csye6225/web-app/.env
-        sudo echo PGPASSWORD=${rds.require("password")} >> /opt/csye6225/web-app/.env
-        sudo echo PGDATABASE=${rds.require("dbName")} >> /opt/csye6225/web-app/.env
+        sudo echo PGPASSWORD=${rds.require(
+          "password"
+        )} >> /opt/csye6225/web-app/.env
+        sudo echo PGDATABASE=${rds.require(
+          "dbName"
+        )} >> /opt/csye6225/web-app/.env
         sudo echo PGHOST=${rdsInstance.address} >> /opt/csye6225/web-app/.env
+        sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+            -a fetch-config \
+            -m ec2 \
+            -c file:/opt/csye6225/web-app/configs/cloudwatch-agent-config.json \
+            -s
+        sudo systemctl enable amazon-cloudwatch-agent
+        sudo systemctl start amazon-cloudwatch-agent
         `,
-        instanceType: "t2.micro",
-        subnetId: publicSubnets[0].id,
-        keyName: keyName,
-        vpcSecurityGroupIds: [
-            securityGroup.id,
-        ],
-        rootBlockDevice: {
-            volumeSize: 25,
-            volumeType: "gp2",
-        },
-        disableApiTermination:false,
-        userDataReplaceOnChange:true,
-        dependsOn: [rdsInstance],
-        tags: {
-            Name: "web-app Pulumi",
-        },
-    });
+    instanceType: "t2.micro",
+    subnetId: publicSubnets[0].id,
+    keyName: keyName,
+    iamInstanceProfile: instanceProfile.name,
+    vpcSecurityGroupIds: [securityGroup.id],
+    rootBlockDevice: {
+      volumeSize: 25,
+      volumeType: "gp2",
+    },
+    disableApiTermination: false,
+    userDataReplaceOnChange: true,
+    dependsOn: [rdsInstance],
+    tags: {
+      Name: "web-app Pulumi",
+    },
+  });
+
+  // creating an A record in route53
+  const hostedZone = aws.route53.getZone({ name: route53Config.require("domainName") });
+
+  const aRecord = new route53.Record("aRecord", {
+    zoneId: hostedZone.then((zone: { zoneId: any; }) => zone.zoneId), // Replace with your hosted zone ID
+    name: route53Config.require("domainName"), // Replace with your desired domain/subdomain
+    type: "A",
+    ttl: 300, // Replace with your desired TTL
+    records: [instance.publicIp], // Assumes ec2Instance has a public IP assigned
+    dependsOn: [instance],
+  });
 };
 
 createVpcAndSubnets();
